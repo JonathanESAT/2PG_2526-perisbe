dbad4538efb5fd294d2cbb51f8e91548e1ba89ff
JonathanESAT <perisbe@esat-alumni.com>
2026-01-15 13:45:20 +0100

    Add files via upload

--- T1/T1_00_FragmentShaderPlayground/FSPlayground/shaders/fragment_shader_11.fs
+++ T1/T1_00_FragmentShaderPlayground/FSPlayground/shaders/fragment_shader_11.fs
@@ -9,13 +9,15 @@ out vec4 fragColor;
 
 void main() {
   
-  float num_stripes = 10.0;
-  float frequency = 10.0 * 6.28;
+  float num_stripes = 6.0;
+  float frequency = num_stripes * 6.28;
   vec2 uv = (gl_FragCoord.xy / u_window_size);
 
-  float wave = sin(uv.x * frequency) * 0.5 + 0.5;
-  float stripes = smoothstep(0.6, 0.85, wave);
+  float wave_xy= sin((uv.x + uv.y) * frequency / 2) * 0.5 + 0.5;
+  float stripes_xy = smoothstep(0.6, 0.85, wave_xy);
 
-  fragColor = vec4(stripes, 0.0,0.0,1.0);
+
+
+  fragColor = vec4(stripes_xy, stripes_xy,0.0,1.0);
 
 }
--- T1/T1_00_FragmentShaderPlayground/FSPlayground/shaders/fragment_shader_14.fs
+++ T1/T1_00_FragmentShaderPlayground/FSPlayground/shaders/fragment_shader_14.fs
@@ -10,21 +10,31 @@ out vec4 fragColor;
 void main() {
   
   vec2 uv = gl_FragCoord.xy / u_window_size;
-  vec2 uv_remapped = uv;
-  uv_remapped.x = (uv.x - 0.25) / 0.5;
-  float dist1 = abs(uv_remapped.x - uv.y);
-  float dist2 = abs((uv_remapped.x + uv.y) - 1.0);
+
+  vec2 centro = vec2(0.5,0.6);
+
+  float aspect_ratio = u_window_size.x / u_window_size.y;
+  uv.x *= aspect_ratio;
+  centro.x *= aspect_ratio;
   
-  float line1 = smoothstep( 0.003, 0.0, dist1);
-  float line2 = smoothstep( 0.003, 0.0, dist2);
+  float dist = distance(uv, centro);
 
-  float mask_left = step(0.25, uv.x);
-  float mask_right = step(uv.x, 0.75);
+  float ring_inner = 0.11;
+  float ring_outer = 0.23;
 
-  float mask = mask_left * mask_right;
+  vec3 background_color = vec3(0.9, 0.9, 0.8);
+  vec3 cyan_color = vec3(0.5, 0.85, 0.85);
+  vec3 orange_color = vec3(0.95, 0.55, 0.30);
 
-  float final_result = (line1 + line2) * mask;
+  vec3 final_color = background_color;
 
-  fragColor = vec4(final_result, final_result, 0.0, 1.0);
-  
+  float blend_factor = (dist - ring_inner) / (ring_outer - ring_inner);
+  vec3 ring_color = mix(cyan_color, orange_color, blend_factor);
+
+  float in_ring = step(ring_inner, dist) * (1.0 - step(ring_outer,dist));
+  final_color = mix(background_color, ring_color, in_ring);
+
+  fragColor = vec4(final_color, 1.0);
+
+
 }
